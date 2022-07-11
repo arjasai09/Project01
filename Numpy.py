f23c620e648186273de4b89fb7864e73486214a7
arjasai09 <arjasai09@gmail.com>
2022-07-11 12:03:39 +0530

    Data Modification file created

--- Numpy.py
+++ Numpy.py
@@ -1,9 +1,12 @@
 import numpy as np
-import Pandas as pd
+import pandas as pd
 
-arr = np.array([[12,3,4], [3,4,56])
+arr = np.array([[12,3,4], [3,4,56]])
 arr
 
 print(arr.shape)
 print(arr.ndim)   
 print(len(arr))    
+
+df = pd.DataFrame()
+print(df)
